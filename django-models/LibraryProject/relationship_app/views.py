f56bfbb0fe39edf181a1ae096858653ea9b7f9ba
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-11-17 22:53:11 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from relationship_app.models import Book
+from django.views.generic import DetailView  # Import DetailView
+from .models import Library  # Import Library model
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # Correct the template path
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view for displaying library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Path to the template
+    context_object_name = 'library'  # Name for the context variable in the template
