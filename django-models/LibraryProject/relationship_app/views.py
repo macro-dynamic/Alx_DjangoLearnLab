c6ee81125a89a5bd05742eb76a22466951cfd9cf
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-11-17 23:06:09 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+# relationship_app/views.py
 from django.shortcuts import render
 from django.views.generic.detail import DetailView  # Correct import for DetailView
 from .models import Book, Library  # Ensure that Book and Library models are correctly imported
@@ -12,11 +13,3 @@ class LibraryDetailView(DetailView):
     model = Library  # Specify the model for this view
     template_name = 'relationship_app/library_detail.html'  # Template for the class-based view
     context_object_name = 'library'  # This is the context variable that will be available in the template
-
-    def get_context_data(self, **kwargs):
-        """
-        Adds books related to this library to the context data.
-        """
-        context = super().get_context_data(**kwargs)  # Get the default context from the parent class
-        context['books'] = self.object.books.all()  # Add books related to the specific library to the context
-        return context
